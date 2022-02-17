806610fa12d13b7f9beafb2b73e796e61776dd26
Dan McLean <fluzzarn@gmail.com>
2022-02-16 20:52:38 -0500

    make tachi_api work

--- Sources/tachi-api/tachi_api.swift
+++ Sources/tachi-api/tachi_api.swift
@@ -8,12 +8,15 @@ public class tachi_api {
     
     public var accessToken:String = ""
     let authplugin = AccessTokenPlugin(tokenClosure: {_ in tachi_api.instance.accessToken})
-    let provider = MoyaProvider<TachiService>(plugins: [tachi_api.instance.authplugin])
+    let provider:MoyaProvider<TachiService>?
     public var server = Server.bokutachi
         
-    private init() {}
+    private init() {
+        provider = MoyaProvider<TachiService>(plugins: [tachi_api.instance.authplugin])
+
+    }
     
     public func getMe() -> AnyPublisher<User,Error> {
-        return provider.requestPublisher(.user(server: server, name: "me")).map({$0.data}).decode(type: UserResponse.self, decoder: JSONDecoder()).map({$0.user}).eraseToAnyPublisher()
+        return (provider?.requestPublisher(.user(server: server, name: "me")).map({$0.data}).decode(type: UserResponse.self, decoder: JSONDecoder()).map({$0.user}).eraseToAnyPublisher())!
     }
 }
